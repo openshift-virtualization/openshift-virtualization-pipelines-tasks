3aaa7a17fad79599e9e630f11c485a743c97664e
Karel Simon <ksimon@redhat.com>
2025-01-10 13:39:44 +0100

    chore: bump minimal version to 4.17
    Signed-off-by: Karel Simon <ksimon@redhat.com>

--- check-for-new-release.sh
+++ check-for-new-release.sh
@@ -2,4 +2,4 @@
  
 existing_tags=$(skopeo list-tags docker://registry.redhat.io/container-native-virtualization/kubevirt-tekton-tasks-create-datavolume-rhel9 | jq '.Tags | sort | join(",")')
 
-go run main.go --existing-tags="${existing_tags}" --minimal-version="v4.15" --dry-run=false --repository-url="https://github.com/openshift-virtualization/openshift-virtualization-pipelines-tasks"
+go run main.go --existing-tags="${existing_tags}" --minimal-version="v4.17" --dry-run=false --repository-url="https://github.com/openshift-virtualization/openshift-virtualization-pipelines-tasks"
